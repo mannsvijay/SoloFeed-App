ca929c4545a9768f46c9cf99eb76a060391575bc
mannsvijay <mnvj69@gmail.com>
2026-07-17 23:57:21 +0530

    add initial comments to store.js explaining Redux Toolkit store creation

---
+++ SoloFeed/src/store/store.js
@@ -0,0 +1,3 @@
+// this is used to create a store for the application using Redux Toolkit. It combines all the reducers and middleware
+//  into a single store that can be used throughout the app.
+
